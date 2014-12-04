737ed18f3fad8077e73080186f3764a21ed15824
paperduck <paperduck@users.noreply.github.com>
2014-12-04 16:18:49 -0500

    Works. Fixed SD and File usage.

--- arduino_led_sign_web_server.ino
+++ arduino_led_sign_web_server.ino
@@ -12,11 +12,20 @@ LED Adapter
  - xxx triple-check memory allocation + deallocation
  */
 
+/*
+Notes:
+ 
+ Serial.print( "2\n" ); // costs 17 bytes?
+ file_path_name_bu (char*)malloc( 11 ); // reserves extra 2 bytes of Arduino memory (e.g. malloc(12) -> 14 bytes used)
+ file_path_name_buf = "/index.htm"; // calling this repeatedly than once doesn't consume more memory
+ */
+
 /****************************************************************/
 #include <LiquidCrystal.h>
 #include <SPI.h>
 #include <Ethernet.h>
 #include <SD.h>
+#include <EEPROM.h>
 
 /****************************************************************/
 //Create LCD screen instance ***
@@ -27,17 +36,16 @@ byte mac[] = {
   0x90, 0xA2, 0xDA, 0x0F, 0x96, 0xBE};
 byte ip[] = {
   10, 10, 151, 121};
-unsigned short int server_port = 80;
-EthernetServer server = EthernetServer(server_port);
+EthernetServer server = EthernetServer(80);
 EthernetClient client = EthernetClient();
 
 // SD card ***
-File *                     f; // file to read from SD card
+File                       f; // file to read from SD card
 
 // diagnostic ***
-unsigned short int         num_requests = 0;   // counter for requests
-unsigned long              num_bytes_sent = 0; // counter for num bytes sent
-unsigned long              num_bytes_rec = 0;  // counter for num bytes received
+//unsigned short int         num_requests = 0;   // counter for requests
+//unsigned long              num_bytes_sent = 0; // counter for num bytes sent
+//unsigned long              num_bytes_rec = 0;  // counter for num bytes received
 unsigned short int         status_code = 0;
 String                     serial_debug_string = "";
 unsigned long              time_of_last_serial_debug_print = millis();
@@ -55,21 +63,22 @@ String                     request_type = ""; // GET, POST
 String                     file_path_name = ""; // file requested + args
 char *                     file_path_name_buf; // char* for passing into SD.exists(), SD.open()
 //String                     file_path_name_trimmed = ""; // file requested + args
-String                     http_version_client = ""; // HTTP version specified by client
+//String                     http_version_client = ""; // HTTP version specified by client
 boolean                    appending_token = true;
 long                       index_of; // stores result of String.indexOf()
 
+// EEPROM
 
 /****************************************************************/
-String get_response_header(String response_header_type, File * f) // xxx   Make sure f is a pointer, to preserve memory.
+//boolean send_http_header(String response_header_type, unsigned int file_size)
+boolean send_http_header(unsigned int file_size)
 {  
   // 'response_header_type' should be a media type, e.g. "image/png", "text/html".
   // http://en.wikipedia.org/wiki/Internet_media_type#Type_image
-  String line_1 = String("HTTP/1.1 200 OK\r\n");
-  String line_2 = String("Content-Type: ") + response_header_type + String("\r\n");
-  String line_3 = String("Content-Length: ") + String(f->size()) + String("\r\n");
 
-  return String(line_1) + String(line_2) + String(line_3);
+    server.print( "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-length: " );
+  server.print( String(file_size) );
+  server.print( "\r\n\r\n" );
 }
 
 /****************************************************************/
@@ -123,9 +132,9 @@ void setup()
   delay(d5);
 
   // SD card
-  f = (File *)malloc(sizeof(File));
+  //f = (File *)malloc(sizeof(File));
   pinMode(10, OUTPUT);
-  while ( ! SD.begin(4) )
+  while ( ! SD.begin(4))
   {
     lcd.clear();
     lcd_print(0,0,"SD !!"); 
@@ -135,6 +144,20 @@ void setup()
   lcd_print(0,0,"SD ok"); 
   delay(d5);  
   lcd.clear();
+}
+
+/****************************************************************/
+void refresh_memory_stats()
+{
+  memory_available = availableMemory();
+  if (memory_available < memory_available_min)
+  {
+    memory_available_min = memory_available;
+    lcd_print(0, 0, "    ");
+    lcd_print(0, 1, "    ");
+  }
+  lcd_print(0, 0, String( memory_available )); 
+  lcd_print(0, 1, String( memory_available_min )); 
 }
 
 /****************************************************************/
@@ -148,13 +171,8 @@ void loop()
   //    serial_debug_string = "";
   //  }
 
-  memory_available = availableMemory();
-  if (memory_available < memory_available_min)
-  {
-    memory_available_min = memory_available;
-    lcd_print(0, 0, "    ");
-  }
-  lcd_print(0, 0, String( memory_available_min ));  
+  refresh_memory_stats();
+
   //  if (status_code < 10)
   //  {
   //    lcd_print(14, 1, " "); // clear error code 
@@ -193,12 +211,12 @@ void loop()
               break; 
             case 1:
               // file requested
-              file_path_name = token;
+              file_path_name = token; 
               break; 
             case 2:
               // http version
               // This probably won't be reached because the last token should end with carriage return
-              http_version_client = token;
+              //http_version_client = token;
               break; 
             default:
               // 400 (Bad Request)
@@ -226,7 +244,7 @@ void loop()
             switch(cur_token_index)
             {
             case 2:
-              http_version_client = token;
+              //http_version_client = token;
               break; 
             default:
               // 400 (Bad Request)
@@ -259,7 +277,8 @@ void loop()
         token += String(char(cur_byte));
       }
     }// end while
-    // send response
+
+      // send response
 
       // parse out arguments in file path
     index_of = file_path_name.indexOf("?", 0);
@@ -270,54 +289,58 @@ void loop()
     }
 
     // retrieve the requested file
-    //    file_path_name_buf = (char*)malloc( (sizeof(char) * file_path_name.length()) + 1 );
-    //    file_path_name_buf = (char*)malloc( 11 );
-    //    file_path_name.toCharArray(file_path_name_buf, (file_path_name.length() + 1));
-    //    file_path_name.toCharArray(file_path_name_buf, 11);
-    file_path_name_buf = "/index.htmx";
-    file_path_name_buf[10] = '\n';
-    Serial.print( file_path_name_buf );
-    if (SD.exists(file_path_name_buf))
+    file_path_name_buf = (char*)malloc( (sizeof(char) * file_path_name.length()) + 1 );
+    file_path_name.toCharArray(file_path_name_buf, (file_path_name.length() + 1));
+    //file_path_name_buf[file_path_name.length()] = '\0';
+
+    if (SD.exists(file_path_name_buf)) // costs 119 bytes ?
     {
-      lcd_print(4, 0, "s+");
-      *f = SD.open(file_path_name_buf, FILE_READ);
-      if (f->available())
+      lcd_print(4, 0, "s+");  
+      f = SD.open(file_path_name_buf, FILE_READ);  // costs 31 bytes ?     
+      if (f.available())
       {
         lcd_print(6, 0, "f+");
-        Serial.print( "1\n" ); // costs 17 bytes?
+        // xxx determine content-type based on filename
+        // send header
+        send_http_header(f.size());
+        // send file
+        while (f.available())
+        {
+          server.write(f.read());
+        }
+        // done sending response; discharge client
+        if (client.connected())
+        {
+          client.stop(); 
+          ;
+        }
       }
       else
       {
-        lcd_print(6, 0, "f-");
-        Serial.print( "2\n" ); // costs 17 bytes?
         // xxx  204 No Content 
+        lcd_print(6, 0, "f-");
+        send_http_header(14);
+        server.print("204 no content");
+        client.stop(); 
       }
+      // close file 
+      f.close();
     }
     else
     {
-      lcd_print(4, 0, "s-");
       // xxx  404 Not Found
-    }
-    free(file_path_name_buf); // super-important memory deallocation
-    // send header
-    // server.println( get_response_header("text/html", f ) );
-
-    // send file
-    while (f->available())
-    {
-      server.write(f->read());
-    }
-
-    // done sending response; discharge client
-    if (client.connected())
-    {
+      lcd_print(4, 0, "s-");
+      send_http_header(13);
+      server.print("404 not found");
       client.stop(); 
-      ;
     }
-    else
-    {
-      ;
-    }
+
+    free(file_path_name_buf); // super-important memory deallocation
+    token = "";
+    request_type = "";
+    file_path_name = "";
+    // any others?
+
   }// end if(client)
   else
   {
@@ -328,6 +351,35 @@ void loop()
     client = server.available(); 
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
